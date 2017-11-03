626adace8d693e26f85d9d315d4e2191af4c2331
Miguel Tavares <conserva@gmail.com>
2017-11-03 09:07:45 +0000

    Add better styles

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,6 +2,7 @@ class DosesController < ApplicationController
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
+    @ingredients = Ingredient.order("name ASC")
   end
 
   def create
@@ -18,6 +19,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
   end
 
   private
